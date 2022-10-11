1079787620e571af3aa82edd245e22bcc06f8ded
kasir barati <kasir.barati@gmail.com>
2022-10-11 11:25:39 +0330

    add: footer implemented

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import FooterClasses from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={FooterClasses.footer}>
+      Copyleft by Kasir Barati 2022
+    </footer>
+  );
+}
