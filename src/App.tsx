c442984264bcba3c2b5eafade1faf5b9a9156081
kasir barati <kasir.barati@gmail.com>
2022-10-11 11:26:53 +0330

    update: use new components in the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Menu from "./components/menu/Menu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header></Header>
+      <Menu
+        items={[
+          { text: "WhoAmI", pageUrl: "#", isActive: true },
+          { text: "WhereAmI", pageUrl: "#" },
+          { text: "LearnSomething", pageUrl: "#" },
+          { text: "ReadSomething", pageUrl: "#" },
+        ]}
+      ></Menu>
+      <Main></Main>
+      <Footer></Footer>
     </div>
   );
 }
